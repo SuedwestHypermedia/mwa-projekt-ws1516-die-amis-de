ceed62029c51cb7a88e12b9d6a167bee6d6efb4f
swh-back-end-dev <adsuin@gmail.com>
2016-02-29 23:00:06 +0100

    Google Translate API implementation.

--- translator.php
+++ translator.php
@@ -8,6 +8,77 @@ include("inc/header.php"); ?>
 
 		<section>
 
+<?php
+    $apiKey = 'AIzaSyDlCChtJMBUatdS16Ags8NnT0FAwbEoTGU';
+    $text = 'Ich libe dich!';
+    $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=de&target=en';
+
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($handle);
+    $responseDecoded = json_decode($response, true);
+    $responseCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);      //Here we fetch the HTTP response code
+    curl_close($handle);
+
+    if($responseCode != 200) {
+        echo 'Fetching translation failed! Server response code:' . $responseCode . '<br>';
+        echo 'Error description: ' . $responseDecoded['error']['errors'][0]['message'];
+    }
+    else {
+        echo 'Source: ' . $text . '<br>';
+        echo 'Translation: ' . $responseDecoded['data']['translations'][0]['translatedText'];
+    }
+?>
+
+<br>
+<br>
+
+<?php
+    $apiKey = 'AIzaSyDlCChtJMBUatdS16Ags8NnT0FAwbEoTGU';
+    $text = 'Hello world!';
+    $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=de&target=en';
+
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($handle);
+    $responseDecoded = json_decode($response, true);
+    $responseCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);      //Here we fetch the HTTP response code
+    curl_close($handle);
+
+    if($responseCode != 200) {
+        echo 'Fetching translation failed! Server response code:' . $responseCode . '<br>';
+        echo 'Error description: ' . $responseDecoded['error']['errors'][0]['message'];
+    }
+    else {
+        echo 'Source: ' . $text . '<br>';
+        echo 'Translation: ' . $responseDecoded['data']['translations'][0]['translatedText'];
+    }
+?>
+
+<br>
+<br>
+
+<?php
+    $apiKey = 'AIzaSyDlCChtJMBUatdS16Ags8NnT0FAwbEoTGU';
+    $text = 'Wie heißt du?';
+    $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=de&target=en';
+
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($handle);
+    $responseDecoded = json_decode($response, true);
+    $responseCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);      //Here we fetch the HTTP response code
+    curl_close($handle);
+
+    if($responseCode != 200) {
+        echo 'Fetching translation failed! Server response code:' . $responseCode . '<br>';
+        echo 'Error description: ' . $responseDecoded['error']['errors'][0]['message'];
+    }
+    else {
+        echo 'Source: ' . $text . '<br>';
+        echo 'Translation: ' . $responseDecoded['data']['translations'][0]['translatedText'];
+    }
+?>
 		</section>
 
 </div>
